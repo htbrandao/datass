a67506d37cbf8380527cf07aab4aa2e828661af7
hbrandao <brandao.t.henrique@gmail.com>
2020-12-24 13:50:33 -0300

    checkpoint

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/hbrandao/data-science-shortcuts",
+    download_url="https://gitlab.com/hbrandao/data-science-shortcuts/dist/datass-0.0.1.tar.gz"
     packages=setuptools.find_packages(),
     classifiers=[
         "Intended Audience :: Data Science",
