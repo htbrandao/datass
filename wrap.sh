8fc6672bd84e9a77c2b108a0ca41ff75ff995c49
hbrandao <brandao.t.henrique@gmail.com>
2020-12-24 14:22:26 -0300

    wrapping version

--- wrap.sh
+++ wrap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-git add .
+echo "Cleaning old build"
+rm -vr dist/
 
-git status
-
-git commit -m 'wrapping version'
-
-git push origin master
-
+echo "Creating new build"
 python3 setup.py sdist bdist_wheel
-
 twine upload --verbose dist/*
+
+echo "Publishing to Git"
+git add .
+git status
+git commit -m 'wrapping version'
+git push origin master
